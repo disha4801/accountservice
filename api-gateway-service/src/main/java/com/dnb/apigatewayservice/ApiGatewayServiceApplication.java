9f2817331ca5e09e15c2aa82fca1249c825461e7
sainid <sainid@192.168.1.31>
2023-09-29 22:32:14 +0530

    commit security

--- api-gateway-service/src/main/java/com/dnb/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/dnb/apigatewayservice/ApiGatewayServiceApplication.java
@@ -3,6 +3,10 @@ package com.dnb.apigatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+
+import reactor.core.publisher.Mono;
 
 
 @EnableDiscoveryClient
@@ -11,6 +15,11 @@ public class ApiGatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+	}
+	
+	@Bean
+	KeyResolver userKeySolver() {
+		return exchange -> Mono.just("userKey");
 	}
 
 }
